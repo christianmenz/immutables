b77c7d4008b594e4fa0263b41214e35601ef59a5
Christian Menz <christianmenz@gmail.com>
2018-12-25 07:44:08 +0100

    added from() example

--- src/main/java/ch/christianmenz/immutables/Main.java
+++ src/main/java/ch/christianmenz/immutables/Main.java
@@ -6,5 +6,8 @@ public class Main {
         ImmutableOrder.Builder builder = ImmutableOrder.builder();
         ImmutableInstrument instrument = ImmutableInstrument.builder().identifier("instr1").name("Tesla").build();
         ImmutableOrder order = builder.name("Test").instrument(instrument).identifier("ord1").build();
+
+        // Build a new order based on existing order
+        ImmutableOrder order1 = ImmutableOrder.builder().from(order).name("Test2").build();
     }
 }
